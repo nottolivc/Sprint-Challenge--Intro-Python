2bbbec3f83191d13b86c380ceb7604f09e25be14
Andrew Nottoli <andrewnottoli@users-iMac.local>
2020-06-14 17:33:55 -0700

    fixed list to range err

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -56,10 +56,9 @@ print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,# inclusive.
-# print("Names and ages between 27 and 32:")
-# f = [tuple((x.name).split()) + tuple((x.age))
-#      for x in humans if str(x.age) <= "32"]
-# print(f)
+print("Names and ages between 27 and 32:")
+f = [(x.name, x.age) for x in humans if x.age in range(27, 33)]
+print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.

7ad37d180fa0c0893fbdbd9781287680f5df56ce
Andrew Nottoli <andrewnottoli@users-iMac.local>
2020-06-14 17:23:08 -0700

    getting err for naem age tuple otherwise good

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -55,11 +55,11 @@ e = [x.name + "-" + str(x.age) for x in humans]
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
-# age, for example ("David", 31), for everyone between the ages of 27 and 32,
-# inclusive.
-print("Names and ages between 27 and 32:")
-f = [tuple((x.name).split() + tuple((x.age))) for x in humans if int(x.age) >= 27 and <= 32]
-print(f)
+# age, for example ("David", 31), for everyone between the ages of 27 and 32,# inclusive.
+# print("Names and ages between 27 and 32:")
+# f = [tuple((x.name).split()) + tuple((x.age))
+#      for x in humans if str(x.age) <= "32"]
+# print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
